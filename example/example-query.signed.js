dc7a3e95f34e165fed91c09f5729caa5c8cce573
gosho-kazuya <ketsume0211@gmail.com>
2021-04-03 17:25:48 +0900

    fix example

--- example/example-query.signed.js
+++ example/example-query.signed.js
@@ -1,19 +1,7 @@
-const gql = literals => literals[0]
+const gql = literals => literals[0];
 
-export const getUsers = gql`
-  query getUsers {
-    name
-    email
-  }
-`
-
-export const updateUser = gql`
-  query updateUser(id: 1, name: "Bob") {
-    name
-    email
-  }
-`
-
+export const getUsers = gql`eyJhbGciOiJIUzI1NiJ9.CiAgcXVlcnkgZ2V0VXNlcnMgewogICAgbmFtZQogICAgZW1haWwKICB9Cg.GRFoVNHpY12mX0UI1y_nCRwGqKST4UkAbx88hZ2Jccg`;
+export const updateUser = gql`eyJhbGciOiJIUzI1NiJ9.CiAgcXVlcnkgdXBkYXRlVXNlcihpZDogMSwgbmFtZTogIkJvYiIpIHsKICAgIG5hbWUKICAgIGVtYWlsCiAgfQo.PZF-UCoKT1tCcF8ZXbKB0GVIjLuHako5m8REBSF3cJA`;
 export const notGql = gql`
   Hello, I am just template string
-`
+`;
